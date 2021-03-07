e0bde754616d7c1537ac1c90b427283f17437436
Kim Doberstein <dobe0002@umn.edu>
2021-03-07 14:01:34 -0600

    Add dist to lint ignore

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -12,31 +12,5 @@ import STable from './SortTable';
 //    import MyComponent from 'MyComponent'
 
 const SortTable = (props) => <STable {...props} />;
-// const {
-//   tableData,
-//   headers,
-//   initialSort,
-//   caseSensitiveFilter,
-//   showFilter,
-//   showPagination,
-//   dangerouslySetInnerHTML,
-//   viewSteps,
-//   defaultToAll,
-// } = props;
-
-// return (
-//   <STable
-//     tableData={tableData}
-//     headers={headers}
-//     initialSort={initialSort}
-//     caseSensitiveFilter={caseSensitiveFilter}
-//     showFilter={showFilter}
-//     showPagination={showPagination}
-//     dangerouslySetInnerHTML={dangerouslySetInnerHTML}
-//     viewSteps={viewSteps}
-//     defaultToAll={defaultToAll}
-//   />
-// );
-// };
 
 export default SortTable;
