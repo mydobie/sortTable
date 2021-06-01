6d876165bec7be0860a6896db9af5ecc21466ee5
kimdobie <kimdobie@gmail.com>
2021-05-31 21:42:28 -0500

    Eject and build

--- utils/buildPackage.sh
+++ utils/buildPackage.sh
@@ -1,10 +1,7 @@
-distFolder=lib #IF this changes ... also change minifyFilesInDir.js and restPackageJson.js
 
 NODE_ENV=production
-rm -rf $distFolder
-mkdir $distFolder
 npm run buildts
 node utils/resetPackageJson.js
 node utils/minifyFilesInDir.js
 
-npm pack ./$distFolder
+npm pack ./lib
