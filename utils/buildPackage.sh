5242090fdf2fe15dc892ed08d7bb4f02bd83ce49
kimdobie <kimdobie@gmail.com>
2021-05-31 21:13:01 -0500

    WIP: mid change files

--- utils/buildPackage.sh
+++ utils/buildPackage.sh
@@ -1,9 +1,9 @@
-distFolder=dist #IF this changes ... also change minifyFilesInDir.js and restPackageJson.js
+distFolder=lib #IF this changes ... also change minifyFilesInDir.js and restPackageJson.js
 
 NODE_ENV=production
 rm -rf $distFolder
-mkdir dist
-npx babel src/Components --out-dir $distFolder --copy-files
+mkdir $distFolder
+npm run buildts
 node utils/resetPackageJson.js
 node utils/minifyFilesInDir.js
 
