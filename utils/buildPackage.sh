5491816392f0a49d019be48f0741702cf800a744
kimdobie <kimdobie@gmail.com>
2021-06-08 21:15:07 -0500

    Revert eject

--- utils/buildPackage.sh
+++ utils/buildPackage.sh
@@ -1,8 +1,11 @@
+distFolder=lib #IF this changes ... also change minifyFilesInDir.js and restPackageJson.js
 
 NODE_ENV=production
+rm -rf $distFolder
+mkdir $distFolder
 npm run buildts
 node utils/resetPackageJson.js
 node utils/moveCss.js
 node utils/minifyFilesInDir.js
 
-npm pack ./lib
+npm pack ./$distFolder
