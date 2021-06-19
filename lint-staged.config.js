596abc05ba7ee732d9b988022241fb33a4c51314
kimdobie <kimdobie@gmail.com>
2021-06-19 16:19:55 -0500

    Fix issue where lint staged was not run on commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,11 @@
 // Sets scripts run as part of Husky's Git commit hooks.  There normally isn't a need to change this file
 
 module.exports = {
-  'src/**/*.{js,jsx}': [
+  'src/**/*.{ts, tsx, js,jsx}': [
     (jsFiles) => jsFiles.map((jsFile) => `npm run lint:js ${jsFile}`), // check files
   ],
 
-  'src/**/*.html': [
+  'src/**/*.{html,md,json}': [
     (htmlFiles) => htmlFiles.map((htmlFile) => `npm run prettier ${htmlFile}`), // check files
   ],
 };
