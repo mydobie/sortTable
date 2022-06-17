dd2a4d25bf364da9becddea66f60f26180338a7d
kimdobie <kimdobie@gmail.com>
2022-06-16 19:41:23 -0500

    Don't lint scss files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,9 +2,7 @@ module.exports = {
   'src/**/*.{ts,tsx,js,jsx}': [
     (jsFiles) => jsFiles.map((jsFile) => `npm run lint:js ${jsFile}`), // check files
   ],
-  'src/**/*.{scss,css}': [
-    (scssFiles) => scssFiles.map((scssFile) => `npm run lint:scss ${scssFile}`), // check files
-  ],
+
   'src/**/*.{html,json,md}': [
     (htmlFiles) => htmlFiles.map((htmlFile) => `npm run prettier ${htmlFile}`), // check files
   ],
